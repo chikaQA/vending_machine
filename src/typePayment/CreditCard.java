1a27231a9cdf05e1374737d4b8c9700408841c02
chyngyz_T <talaibekovchyngyz3@gmail.com>
2025-09-19 13:56:17 +0600

    fix bugs

--- src/typePayment/CreditCard.java
+++ src/typePayment/CreditCard.java
@@ -1,6 +1,4 @@
 package typePayment;
-
-import helper.HelperMethod;
 import payment.Payment;
 
 import java.util.Random;
@@ -19,88 +17,90 @@ public class CreditCard implements Payment {
     }
 
 
-
-    public static void validateCardNumber(CreditCard creditCard){
-        while (creditCard.cardNumber==null) {
+    public static void validateCardNumber(CreditCard creditCard) {
+        while (creditCard.cardNumber == null) {
             print("Введите номер карты: ");
             String cardNumber2 = fromConsole();
-            if(!cardNumber2.isEmpty()&&!cardNumber2.isBlank()){
-                if(cardNumber2.length()==16){
-                    try{
-                        creditCard.setCardNumber(String.valueOf(Long.parseLong(cardNumber2)));
-                    }catch (Exception e){
-                        print("Введите корректные данные");
-                    }
-
-                }else{
-                    print("Карта должна быть 16 значений вы ввели "+cardNumber2.length());
-                }
-            }else {
-                print("Поле не может быть пустым. Введите данные корректно");
-            }
-        }
-    }
-    public static void validateValidityPeriod(CreditCard creditCard){
-        while (creditCard.validityPeriod==null) {
-            print("Введите срок действия карты [месяц/год]: ");
-            String period = fromConsole();
-            if(!period.isEmpty()&&!period.isBlank()){
-                try{
-                    String[] validityPeriod = period.split("/");
-                    if(validityPeriod.length==2&&Integer.parseInt(validityPeriod[1])>25&&Integer.parseInt(validityPeriod[0])<12){
-                        creditCard.setValidityPeriod(period);
-                    }else {
-                        print("Введите корректные данные");
-                    }
-                }catch (ArithmeticException e){
-                    print("Введите корректные данные");
-                }
-            }else {
-               print("Поле не может быть пустым. Введите данные корректно");
-            }
-        }
-    }
-    public static void validateCVV(CreditCard creditCard){
-        while (creditCard.cvv==null) {
-            HelperMethod.print("Введите cvv код на задней части экрана: ");
-            String cvv = HelperMethod.fromConsole();
-            if(!cvv.isEmpty()&&!cvv.isBlank()){
-                if (cvv.length()==3) {
+            if (!cardNumber2.isEmpty() && !cardNumber2.isBlank()) {
+                if (cardNumber2.length() == 16) {
                     try {
-                        Integer.parseInt(cvv);
-                         creditCard.setCvv(cvv);
+                        creditCard.setCardNumber(String.valueOf(Long.parseLong(cardNumber2)));
                     } catch (Exception e) {
                         print("Введите корректные данные");
                     }
-                }else {
-                    print("Введите трехзначное число корректно");
+
+                } else {
+                    print("Карта должна быть 16 значений вы ввели " + cardNumber2.length());
                 }
-            }else {
+            } else {
                 print("Поле не может быть пустым. Введите данные корректно");
             }
         }
     }
 
-    public static int validateOTP(CreditCard creditCard){
-        print("Введите SMS-код из 5 цифр, отправленыый на ваш мобильный номер");
+    public static void validateValidityPeriod(CreditCard creditCard) {
+        while (creditCard.validityPeriod == null) {
+            print("Введите срок действия карты [месяц/год]: ");
+            String period = fromConsole();
+            if (!period.isEmpty() && !period.isBlank()) {
+                try {
+                    String[] validityPeriod = period.split("/");
+                    if (validityPeriod.length == 2 && Integer.parseInt(validityPeriod[1]) > 25 && Integer.parseInt(validityPeriod[0]) < 12) {
+                        creditCard.setValidityPeriod(period);
+                    } else {
+                        print("Введите корректные данные");
+                    }
+                } catch (ArithmeticException e) {
+                    print("Введите корректные данные");
+                }
+            } else {
+                print("Поле не может быть пустым. Введите данные корректно");
+            }
+        }
+    }
+
+    public static void validateCVV(CreditCard creditCard) {
+        while (creditCard.cvv == null) {
+            print("Введите cvv код на задней части экрана: ");
+            String cvv = fromConsole();
+            if (!cvv.isEmpty() && !cvv.isBlank()) {
+                if (cvv.length() == 3) {
+                    try {
+                        Integer.parseInt(cvv);
+                        creditCard.setCvv(cvv);
+                    } catch (Exception e) {
+                        print("Введите корректные данные");
+                    }
+                } else {
+                    print("Введите трехзначное число корректно");
+                }
+            } else {
+                print("Поле не может быть пустым. Введите данные корректно");
+            }
+        }
+    }
+
+    public static int validateOTP(CreditCard creditCard) {
+        print("Для подтверждения оплаты введите SMS-код из 5 цифр, отправленыый на ваш мобильный номер");
         Random random = new Random();
-        int otpCode =random.nextInt(10000,99999);
-        print("Вам пришло SMS-для оплаты "+otpCode);
+        int otpCode = random.nextInt(10000, 99999);
+        print("Вам пришло SMS-для оплаты " + otpCode);
         while (true) {
-            try{
+            try {
                 String otp = fromConsole();
-                int otpC = Integer.parseInt(otp);
-                if(!otp.isEmpty()&&!otp.isBlank()){
-                    if (otp.length()==5&&otpC==otpCode) {
-                       return otpC;
-                    }else {
+                int otpSms = Integer.parseInt(otp);
+                if (!otp.isEmpty() && !otp.isBlank()) {
+                    if (otp.length() == 5 && otpSms == otpCode) {
+                        creditCard.setOtp(otpSms);
+                        return otpSms;
+                    } else {
                         print("Неправильно введен ОТП код ");
                     }
-                }else {
+                } else {
                     print("Поле не может быть пустым. Введите данные корректно");
                 }
-            }catch (Exception e){
-               print("Неправильно введен ОТП код");
+            } catch (Exception e) {
+                print("Неправильно введен ОТП код");
             }
 
         }
@@ -115,6 +115,7 @@ public class CreditCard implements Payment {
     public void setAmount(int amount) {
         this.amount = amount;
     }
+
     public void setCardNumber(String cardNumber) {
         this.cardNumber = cardNumber;
     }
@@ -126,9 +127,15 @@ public class CreditCard implements Payment {
     public void setCvv(String cvv) {
         this.cvv = cvv;
     }
+
     private static String fromConsole() {
         return new Scanner(System.in).nextLine();
     }
+
+    public void setOtp(int otp) {
+        this.otp = otp;
+    }
+
     private static void print(String msg) {
         System.out.println(msg);
     }
